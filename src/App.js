0c7e8d2379d35d3ead08a664f0cf81051827d371
Filipe <filipeunithing@gmail.com>
2024-09-16 20:58:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { index } from "./index";
+import { useState, useEffect } from "react";
 
 const listaInicial = [
   {
@@ -30,25 +29,61 @@ const listaInicial = [
 
 export default function App() {
   const [tarefas, setTarefas] = useState(listaInicial);
-  // const tarefas = listaInicial;
+  //adicionar mais um state para que ele guarde
+  const [sortBy, setSortBy] = useState("crescente");
+
+  //verifica por categoria
+  const ordenarListaAlfabetica = () => {
+    if (
+      sortBy === "trabalho" ||
+      sortBy === "estudo" ||
+      sortBy === "lazer" ||
+      sortBy === "exercicios"
+    ) {
+      const listaFiltrada = tarefas.filter(
+        (tarefaAtual) =>
+          tarefaAtual.categoria.toLowerCase() === sortBy.toLowerCase()
+      );
+      setTarefas(listaFiltrada);
+      return; // Retorna aqui para evitar continuar a ordenar
+    }
+
+    const listaOrdenada = [...tarefas].sort((a, b) => {
+      if (sortBy === "crescente") {
+        console.log("teste a");
+        return a.nomeTarefa.localeCompare(b.nomeTarefa);
+      }
+      if (sortBy === "decrescente") {
+        console.log("teste b");
+        return b.nomeTarefa.localeCompare(a.nomeTarefa);
+      }
+    });
+
+    //envia a lista atualizada
+    setTarefas(listaOrdenada);
+  };
+  //metodo para corrigir o fato de que setSortBy nao é assincrono, para que garante que a funcao ordernar execute apos atualizar o valor de sortBy
+  useEffect(() => {
+    ordenarListaAlfabetica();
+  }, [sortBy]);
 
   //adicionar
   function adicionarTarefas(novaTarefa) {
-    setTarefas((tarefasAtuais) => [...tarefasAtuais, novaTarefa]); // Corrigido
+    setTarefas((tarefasAtuais) => [...tarefasAtuais, novaTarefa]);
   }
+
   //deletar
   function deletarTarefa(id) {
     setTarefas((tarefasAtuais) =>
-      tarefasAtuais.filter((tarefaAtual) => tarefaAtual.id != id)
+      tarefasAtuais.filter((tarefaAtual) => tarefaAtual.id !== id)
     );
   }
 
-  //verificar domingo
+  //verificar completo
   function completarTarefa(id) {
-    console.log("chegou");
     setTarefas((tarefasAtuais) =>
       tarefasAtuais.map((tarefaAtual) =>
-        tarefaAtual === id
+        tarefaAtual.id === id
           ? { ...tarefaAtual, completado: !tarefaAtual.completado }
           : tarefaAtual
       )
@@ -57,7 +92,10 @@ export default function App() {
 
   return (
     <div className="main">
-      <Pesquisar />
+      <Pesquisar
+        setSortBy={setSortBy}
+        ordenarListaAlfabetica={ordenarListaAlfabetica}
+      />
       <ListaTarefas
         tarefas={tarefas}
         onDeletarTarefa={deletarTarefa}
@@ -68,31 +106,27 @@ export default function App() {
   );
 }
 
-function Pesquisar() {
+function Pesquisar({ setSortBy }) {
   return (
     <div className="pesquisar">
+      <h1>Pesquisar</h1>
+      <input placeholder="Digite para pesquisar"></input>
+      <h1>Filtrar</h1>
+      <select onChange={(e) => setSortBy(e.target.value)}>
+        <option value="todas">Todas</option>
+        <option value="trabalho">Trabalho</option>
+        <option value="estudo">Estudo</option>
+        <option value="lazer">Lazer</option>
+        <option value="exercicios">Exercicios Fisicos</option>
+      </select>
       <div>
-        <h1>Pesquisar</h1>
-        <input placeholder="Digite para pesquisar"></input>
-      </div>
-      <div>
-        <div>
-          <h1>Filtrar</h1>
-          <label>status:</label>
-          <select>
-            <option>Todas</option>
-            <option>Trabalho</option>
-            <option>Estudo</option>
-            <option>Lazer</option>
-            <option>Exercicios Fisicos</option>
-          </select>
-        </div>
-
-        <div>
-          <label>Ordem alfabetica</label>
-          <button>crescente</button>
-          <button>Decrescente</button>
-        </div>
+        <label>Ordem alfabetica</label>
+        <button value="crescente" onClick={(e) => setSortBy(e.target.value)}>
+          Crescente
+        </button>
+        <button value="decrescente" onClick={(e) => setSortBy(e.target.value)}>
+          Decrescente
+        </button>
       </div>
     </div>
   );
@@ -121,14 +155,14 @@ function Tarefas({ tarefa, onDeletarTarefa, onCompletarTarefa }) {
       <p>{tarefa.nomeTarefa}</p>
       <p>{tarefa.categoria}</p>
       <button onClick={() => onCompletarTarefa(tarefa.id)}>Completar</button>
-      <button onClick={() => onDeletarTarefa(tarefa.id)}>fechar</button>
+      <button onClick={() => onDeletarTarefa(tarefa.id)}>Fechar</button>
     </li>
   );
 }
 
 function CriarTarefa({ onAdicionarTarefas }) {
   const [nomeTarefa, setNomeTarefa] = useState("");
-  const [categoria, setCategaoria] = useState("Trabalho");
+  const [categoria, setCategoria] = useState("Trabalho");
   const completado = false;
 
   function adicionarTarefasSubmit(e) {
@@ -140,7 +174,7 @@ function CriarTarefa({ onAdicionarTarefas }) {
     onAdicionarTarefas(novaTarefa);
 
     setNomeTarefa("");
-    setCategaoria("");
+    setCategoria("Trabalho");
   }
 
   return (
@@ -150,16 +184,16 @@ function CriarTarefa({ onAdicionarTarefas }) {
         <input
           value={nomeTarefa}
           onChange={(e) => setNomeTarefa(e.target.value)}
-          placeholder="escreva a tarefa"
+          placeholder="Escreva a tarefa"
         ></input>
         <select
           value={categoria}
-          onChange={(e) => setCategaoria(e.target.value)}
+          onChange={(e) => setCategoria(e.target.value)}
         >
           <option>Trabalho</option>
           <option>Estudo</option>
           <option>Lazer</option>
-          <option>Exercicios Fisicos</option>
+          <option>Exercícios físicos</option>
         </select>
         <button>Criar Tarefa</button>
       </form>
